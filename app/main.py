e831b1b6a9b51ad0d96629b4ddc66155b79d93d5
Nick <esp.m1k@gmail.com>
2024-09-21 13:29:58 -0500

    psql updates

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 import psycopg2 as pgsql
+import sys
+
+sys.tracebacklimit=0
+
+
 
 conn = pgsql.connect(database = "postgres",
                      user     = "postgres",
@@ -6,31 +11,67 @@ conn = pgsql.connect(database = "postgres",
                      host     = "localhost",
                      port     = 5432)
 
-cursor = conn.cursor()
 q_createTable = """CREATE TABLE guest_book(
                firstName VARCHAR(50) NOT NULL,
                lastName VARCHAR(50) NOT NULL,
                timestamp date NOT NULL);
                """
 
+q_deleteTable = """DROP TABLE guest_book;"""
+
+def run_query(query):
+    cursor = conn.cursor()
+    try:
+        cursor.execute(query)
+        response = cursor.fetchall()
+        conn.commit()   
+        cursor.close()
+    except pgsql.OperationalError as e:
+        print(e)
+    
+    return response
+
+def delete_table(query):
+    try:
+        cursor.execute(query)
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except pgsql.OperationalError as e:
+        print(f'Error: {e}')
+
+
 def create_database(query):
     
     try:
         cursor.execute(query)
         conn.commit()
-        output = cursor.fetchall() 
         cursor.close()
         conn.close()
     except pgsql.OperationalError as e:
-        print(e)
+        print("Error: ", e)
 
-    return output
+
+
+def foo():
+    # Delete database
+    try:
+        response = delete_table(q_deleteTable)
+    except:
+        response = print("didnt work")
+    
+    return response
 
 def main():
     print(f'Creating database...')
-    create_database(q_createTable)
-    response = create_database()
+    try:
+        create_database(q_createTable)
+        response = create_database()
+    except pgsql.OperationalError as e:
+        response = print("Unable to create DB.", e)
 
-    return response
+        return response
 
-main()
+
+#foo()
+#main()
